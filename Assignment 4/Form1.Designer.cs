c37dbee1400ea33bb88681543b4f2a1a7f3fd62b
Jaden Whitt <jadenmike31@gmail.com>
2026-01-29 15:29:52 -0500

    Add files via upload

---
+++ Assignment 4/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Assignment_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.onePictureBox = new System.Windows.Forms.PictureBox();
+            this.twoPictureBox = new System.Windows.Forms.PictureBox();
+            this.threePictureBox = new System.Windows.Forms.PictureBox();
+            this.fourPictureBox = new System.Windows.Forms.PictureBox();
+            this.fivePictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.onePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fourPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fivePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // onePictureBox
+            // 
+            this.onePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("onePictureBox.Image")));
+            this.onePictureBox.Location = new System.Drawing.Point(36, 35);
+            this.onePictureBox.Name = "onePictureBox";
+            this.onePictureBox.Size = new System.Drawing.Size(65, 118);
+            this.onePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.onePictureBox.TabIndex = 0;
+            this.onePictureBox.TabStop = false;
+            this.onePictureBox.Click += new System.EventHandler(this.onePictureBox_Click);
+            // 
+            // twoPictureBox
+            // 
+            this.twoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("twoPictureBox.Image")));
+            this.twoPictureBox.Location = new System.Drawing.Point(107, 35);
+            this.twoPictureBox.Name = "twoPictureBox";
+            this.twoPictureBox.Size = new System.Drawing.Size(63, 118);
+            this.twoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.twoPictureBox.TabIndex = 2;
+            this.twoPictureBox.TabStop = false;
+            this.twoPictureBox.Click += new System.EventHandler(this.twoPictureBox_Click);
+            // 
+            // threePictureBox
+            // 
+            this.threePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("threePictureBox.Image")));
+            this.threePictureBox.Location = new System.Drawing.Point(176, 35);
+            this.threePictureBox.Name = "threePictureBox";
+            this.threePictureBox.Size = new System.Drawing.Size(63, 118);
+            this.threePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.threePictureBox.TabIndex = 3;
+            this.threePictureBox.TabStop = false;
+            this.threePictureBox.Click += new System.EventHandler(this.threePictureBox_Click);
+            // 
+            // fourPictureBox
+            // 
+            this.fourPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("fourPictureBox.Image")));
+            this.fourPictureBox.Location = new System.Drawing.Point(245, 35);
+            this.fourPictureBox.Name = "fourPictureBox";
+            this.fourPictureBox.Size = new System.Drawing.Size(63, 118);
+            this.fourPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fourPictureBox.TabIndex = 4;
+            this.fourPictureBox.TabStop = false;
+            this.fourPictureBox.Click += new System.EventHandler(this.fourPictureBox_Click);
+            // 
+            // fivePictureBox
+            // 
+            this.fivePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("fivePictureBox.Image")));
+            this.fivePictureBox.Location = new System.Drawing.Point(314, 35);
+            this.fivePictureBox.Name = "fivePictureBox";
+            this.fivePictureBox.Size = new System.Drawing.Size(63, 118);
+            this.fivePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fivePictureBox.TabIndex = 5;
+            this.fivePictureBox.TabStop = false;
+            this.fivePictureBox.Click += new System.EventHandler(this.fivePictureBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 185);
+            this.Controls.Add(this.fivePictureBox);
+            this.Controls.Add(this.fourPictureBox);
+            this.Controls.Add(this.threePictureBox);
+            this.Controls.Add(this.twoPictureBox);
+            this.Controls.Add(this.onePictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.onePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fourPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fivePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox onePictureBox;
+        private System.Windows.Forms.PictureBox twoPictureBox;
+        private System.Windows.Forms.PictureBox threePictureBox;
+        private System.Windows.Forms.PictureBox fourPictureBox;
+        private System.Windows.Forms.PictureBox fivePictureBox;
+    }
+}
+
